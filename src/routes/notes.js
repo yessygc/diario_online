df0b26336f8d8018cfa7209470f512bbadbcb13e
yessygc <yessgarciacampos@gmail.com>
2021-05-24 20:59:00 -0500

    modificando contenido de archivos

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-note')
+});
+
+router.post('/notes/new-note', (req, res) => {
+    console.log(req.body);
+    res.send('ok');
 })
 
 router.get('/notes', (req, res) => {
